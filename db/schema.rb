5b99bc1559c767ccf6dc65babafaf0bcaf3aa7d6
gurshant <gurshant53@gmail.com>
2020-04-25 03:05:52 -0700

    added users and sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_25_061516) do
+ActiveRecord::Schema.define(version: 2020_04_25_094752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,5 +31,12 @@ ActiveRecord::Schema.define(version: 2020_04_25_061516) do
     t.index ["category_id"], name: "index_expenses_on_category_id"
   end
 
-  add_foreign_key "expenses", "categories" 
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "expenses", "categories"
 end
